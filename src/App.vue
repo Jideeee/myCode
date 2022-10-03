82edf6a9bce79bf8e3e3dd0abbafd1d6b616b610
Jideeee <a15361472569@163.com>
2022-10-03 22:44:19 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class='container'>
+    <Category title='美食'>
+      <img slot="center" src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg">
+      <a slot="footer" class="foot" href="www.baidu.com">更多美食</a>
+    </Category>
+    <Category title='游戏'>
+      <ul slot="center">
+        <li v-for='(g,index) in games' :key='index'>{{g}}</li>
+      </ul>
+      <div slot="footer" class="foot">
+      <a href="baidu.com">单机游戏</a>
+      <a href="baidu.com">网络游戏</a>
+      </div>
+    </Category>
+     <Category title='电影'>
+      <video slot="footer" controls src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+       <div slot="footer" class="foot">
+      <a href="baidu.com">科幻</a>
+      <a href="baidu.com">恐怖</a>
+      <a href="baidu.com">动漫</a>
+      <a href="baidu.com">欧美</a>
+      </div>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from './components/Category.vue'
+export default {
+  name: 'App',
+  components: {
+    Category,
+  },
+  data(){
+    return{
+      foods:['火锅','烧烤','小龙虾','牛排'],
+      games:['红色警戒','穿越火线','劲舞团','超级玛丽'],
+      films:['《教父》','《拆弹专家》','《你好，李焕英》','《尚硅谷》']
+    }
+  },
+}
+</script>
+
+<style>
+ .container,.foot{
+  display: flex;
+  justify-content: space-around;
+ }
+
+ h3{
+  text-align: center;
+  background-color: orange;
+ }
+</style>
